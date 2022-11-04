568ca269afb876f57b253047a7d9bc85877d0a51
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-11-04 11:58:30 +0700

    Create initial data directory

--- main.go
+++ main.go
@@ -5,9 +5,27 @@ import (
 	"net/http"
 
 	"github.com/erikrios/my-story-dummy/controller"
+	cfs "github.com/erikrios/my-story-dummy/util/fs"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+var fs cfs.FS = cfs.NewLocalFS()
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	if !fs.IsExists("data") {
+		if err := fs.CreateDir("data"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if !fs.IsExists("data/.gitkeep") {
+		if err := fs.CreateFile("data/.gitkeep"); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -18,6 +36,13 @@ func main() {
 
 	helloController.Route(r)
 	customController.Route(r)
+
+	apiRouter := chi.NewRouter()
+
+	groupController := controller.NewGroupController()
+	groupController.Route(apiRouter)
+
+	r.Mount("/api/v1", apiRouter)
 
 	port := ":3000"
 	log.Printf("Server starting on port %s\n", port)
